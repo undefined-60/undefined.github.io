c87dc804f92dfbde9b26b1bcca7878ccb21a1014
undefined-60 <108310586+undefined-60@users.noreply.github.com>
2022-11-22 03:35:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const axios = require('axios');
 const fs = require('fs-extra');
-{
-	(async () => {
-		const request = await axios.get('https://api.waifu.pics/sfw/waifu');
-		fs.writeFileSync('request.json', JSON.stringify(request.data, null, 2));
-		})();
-		};
+let d = new Date();
+document.body.innerHTML = "<h1>Time right now is:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()</h1>"
+//{
+	//(async () => {
+		//const request = await axios.get('https://api.waifu.pics/sfw/waifu');
+		//fs.writeFileSync('request.json', JSON.stringify(request.data, null, 2));
+		//})();
+		//};
