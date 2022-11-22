aa0e45bc8b13b91c108654de2cf207b59894112c
undefined-60 <108310586+undefined-60@users.noreply.github.com>
2022-11-22 03:37:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const axios = require('axios');
-const fs = require('fs-extra');
+//const axios = require('axios');
+//const fs = require('fs-extra');
 let d = new Date();
 document.body.innerHTML = "<h1>Time right now is:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()</h1>"
 //{
